fcd8218685037c788939f276d2b4a3a85f99eaba
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-27 20:38:37 -0300

    adicionando funcao vincula

--- app/src/main/java/br/com/fiap/orgs/ui/recycleview/adapter/ListaProdutosAdpter.kt
+++ app/src/main/java/br/com/fiap/orgs/ui/recycleview/adapter/ListaProdutosAdpter.kt
@@ -1,14 +1,29 @@
 package br.com.fiap.orgs.ui.recycleview.adapter
 
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import br.com.fiap.orgs.R
 import br.com.fiap.orgs.model.Produto
 
 class ListaProdutosAdapter(private val produtos: List<Produto>) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+            fun vincula(produto: Produto) {
+                val nome = itemView.findViewById<TextView>(R.id.nome)
+                nome.text = produto.nome
+
+                val descricao = itemView.findViewById<TextView>(R.id.descricao)
+                descricao.text = produto.descricao
+
+                val valor = itemView.findViewById<TextView>(R.id.valor)
+                valor.text = produto.valor.toPlainString()
+            }
+        }
     }
 // equivalente em java
 //    @Override
