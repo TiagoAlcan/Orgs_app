db9272efe380b24c7ad2e7446f57a0dce529d858
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-27 20:31:50 -0300

    quarto commit

--- app/src/main/java/br/com/fiap/orgs/MainActivity.kt
+++ app/src/main/java/br/com/fiap/orgs/MainActivity.kt
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import br.com.fiap.orgs.ui.recycleview.adapter.ListaProdutosAdapter
 
 
 class MainActivity : Activity () {
@@ -15,14 +17,17 @@ class MainActivity : Activity () {
         //Binding: processo e vinculação do
         //codigo fonte com o arquivo de layout.
 
-        val nome = findViewById<TextView>(R.id.nome)
-        //nome.setText("Cesta de frutas")
-        nome.text = "Cesta de frutas"
+//        val nome = findViewById<TextView>(R.id.nome)
+//        //nome.setText("Cesta de frutas")
+//        nome.text = "Cesta de frutas"
+//
+//        val descricao = findViewById<TextView>(R.id.descricao)
+//        descricao.text = "Laranja, manga e uva"
+//
+//        val valor = findViewById<TextView>(R.id.valor)
+//        valor.text = "19.99"
 
-        val descricao = findViewById<TextView>(R.id.descricao)
-        descricao.text = "Laranja, manga e uva"
-
-        val valor = findViewById<TextView>(R.id.valor)
-        valor.text = "19.99"
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = ListaProdutosAdapter();
     }
 }
