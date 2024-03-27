a5316c8771c1b71200c840a2959a4f4529d8d7f3
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-27 20:47:55 -0300

    arrumando funcao vincula e adicionando conteúdo na Main Activiy

--- app/src/main/java/br/com/fiap/orgs/MainActivity.kt
+++ app/src/main/java/br/com/fiap/orgs/MainActivity.kt
@@ -5,7 +5,9 @@ import android.app.Activity
 import android.os.Bundle
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import br.com.fiap.orgs.model.Produto
 import br.com.fiap.orgs.ui.recycleview.adapter.ListaProdutosAdapter
+import java.math.BigDecimal
 
 
 class MainActivity : Activity () {
@@ -14,20 +16,25 @@ class MainActivity : Activity () {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Binding: processo e vinculação do
-        //codigo fonte com o arquivo de layout.
-
+        // Binding: processo de vinculação do código fonte com o arquivo de layout.
 //        val nome = findViewById<TextView>(R.id.nome)
-//        //nome.setText("Cesta de frutas")
+//        nome.setText("Cesta de frutas") ou
 //        nome.text = "Cesta de frutas"
-//
 //        val descricao = findViewById<TextView>(R.id.descricao)
 //        descricao.text = "Laranja, manga e uva"
-//
 //        val valor = findViewById<TextView>(R.id.valor)
 //        valor.text = "19.99"
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.adapter = ListaProdutosAdapter();
+        recyclerView.adapter = ListaProdutosAdapter(
+            context = this,
+            produtos = listOf(
+                Produto("Teste 0", "Teste Descrição 0", BigDecimal("19.90")),
+                Produto("Teste 1", "Teste Descrição 1", BigDecimal("29.90")),
+                Produto("Teste 2", "Teste Descrição 2", BigDecimal("39.90")),
+            )
+        )
+        // Pode ser configurado no layout
+//        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
--- app/src/main/java/br/com/fiap/orgs/ui/recycleview/adapter/ListaProdutosAdpter.kt
+++ app/src/main/java/br/com/fiap/orgs/ui/recycleview/adapter/ListaProdutosAdpter.kt
@@ -1,5 +1,7 @@
 package br.com.fiap.orgs.ui.recycleview.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -7,24 +9,6 @@ import androidx.recyclerview.widget.RecyclerView
 import br.com.fiap.orgs.R
 import br.com.fiap.orgs.model.Produto
 
-class ListaProdutosAdapter(private val produtos: List<Produto>) :
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
-            fun vincula(produto: Produto) {
-                val nome = itemView.findViewById<TextView>(R.id.nome)
-                nome.text = produto.nome
-
-                val descricao = itemView.findViewById<TextView>(R.id.descricao)
-                descricao.text = produto.descricao
-
-                val valor = itemView.findViewById<TextView>(R.id.valor)
-                valor.text = produto.valor.toPlainString()
-            }
-        }
-    }
 // equivalente em java
 //    @Override
 //     public int getItemCount() {
@@ -35,9 +19,39 @@ class ListaProdutosAdapter(private val produtos: List<Produto>) :
 //        return produtos.size
 //    }
 
-    override fun getItemCount(): Int = produtos.size //mesma funcionalidade da de cima (quando temos apenas uma linha da para fazer desse jeito - quando a própria linha é o return da função)
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) { //mesma coisa que o void do java - não retorna nada
-        TODO("Not yet implemented")
+class ListaProdutosAdapter(
+    private val context: Context,
+    private val produtos: List<Produto>
+) : RecyclerView.Adapter<ListaProdutosAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        fun vincula(produto: Produto) {
+            val nome = itemView.findViewById<TextView>(R.id.nome)
+            nome.text = produto.nome
+
+            val descricao = itemView.findViewById<TextView>(R.id.descricao)
+            descricao.text = produto.descricao
+
+            val valor = itemView.findViewById<TextView>(R.id.valor)
+            valor.text = produto.valor.toPlainString()
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ListaProdutosAdapter.ViewHolder {
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.produto_item, parent, false)
+        return ListaProdutosAdapter.ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = produtos.size
+
+    override fun onBindViewHolder(holder: ListaProdutosAdapter.ViewHolder, position: Int) {
+        val produto = produtos[position]
+        holder.vincula(produto)
     }
 }
